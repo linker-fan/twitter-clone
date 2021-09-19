ac5032403a1bae6f9c21184af71eade782d5a8b6
hyperxpizza <wojtek.frackowski@gmail.com>
2021-09-19 09:43:40 +0200

    fix server connection

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const app = express();
 app.use(express.json());
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
-const DATABASE_PORT = process.env.DATABASE_PORT
-const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
-const DATABASE_HOST = process.env.DATABASE_HOST; 
+const DATABASE_PORT = process.env.DATABASE_PORT || 27017;
+const DATABASE_USERNAME = process.env.DATABASE_USERNAME || "root";
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "password";
+const DATABASE_HOST = process.env.DATABASE_HOST || "localhost"; 
 
-mongoose.connect(`mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/admin?authSource=admin&readPreference=primary&gssapiServiceName=mongodb&appname=MongoDB%20Compass&ssl=false`, 
-{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:true })
+mongoose.connect(`mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/admin?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false`, 
+{ useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     console.log(`[+] Connected to the database on port ${DATABASE_PORT}!`);
 })
